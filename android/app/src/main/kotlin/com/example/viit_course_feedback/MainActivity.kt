9627660b7434b0f3aa48bb6bc7ce9059e7e65433
Sampath Balivada <balivadask2000@gmail.com>
2019-12-28 20:49:47 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/viit_course_feedback/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viit_course_feedback
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
