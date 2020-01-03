2d5259844e6cc6adca74868b2f972cef7d567411
Sampath Balivada <balivadask2000@gmail.com>
2020-01-03 11:29:25 +0530

    added data_input_button.dart

---
+++ lib/widgets/data_input_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DetailsButton extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+
+  DetailsButton({@required this.title, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+          color: Theme.of(context).accentColor,
+          disabledColor: Colors.grey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            side: BorderSide(width: 2),
+          ),
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          onPressed: onPressed,
+        );
+      }
+  }
+}
